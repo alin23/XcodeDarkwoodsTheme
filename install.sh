de0f4433073a464625ed359be0c879f8f803c4f8
Alin Panaitiu <alin.panaitiu@gmail.com>
2022-09-23 10:26:00 +0300

    Update

--- install.sh
+++ install.sh
@@ -1,13 +1,19 @@
-#!/bin/sh
+#!/bin/bash
 
-if ! test -f ~/Library/Fonts/iosevka-mayukai-codepro-regular.ttf; then
+if [[ ! -f ~/Library/Fonts/iosevka-mayukai-codepro-regular.ttf ]]; then
     mkdir -p ~/Library/Fonts/
 
-    echo Downloading Iosevka Mayukai Code Pro font...
-    curl -L --progress-bar --output /tmp/iosevka.zip https://github.com/Iosevka-Mayukai/Iosevka-Mayukai/releases/download/v5.9.1/iosevka-mayukai-codepro-editor-v591.zip
-    unzip /tmp/iosevka.zip -d ~/Library/Fonts/
+    if [[ ! -d /tmp/iosevka-mayukai-codepro-editor-v591 ]]; then
+        echo Downloading Iosevka Mayukai Code Pro font to /tmp/iosevka.zip ...
+        curl -L --progress-bar --output /tmp/iosevka.zip https://github.com/Iosevka-Mayukai/Iosevka-Mayukai/releases/download/v5.9.1/iosevka-mayukai-codepro-editor-v591.zip
+    fi
+
+    echo Installing Iosevka Mayukai Code Pro font to ~/Library/Fonts ...
+    unzip -o /tmp/iosevka.zip -d /tmp/
+    cp /tmp/iosevka-mayukai-codepro-editor-v591/ttf/*.ttf ~/Library/Fonts/
 fi
 
+echo Installing Darkwoods.xccolortheme to ~/Library/Developer/Xcode/UserData/FontAndColorThemes ...
 mkdir -p ~/Library/Developer/Xcode/UserData/FontAndColorThemes/
 cp Darkwoods.xccolortheme ~/Library/Developer/Xcode/UserData/FontAndColorThemes/
 
